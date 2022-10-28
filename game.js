2811cb5d9aa3c18747a8fa11e36d3902c591bd21
Noah Segal <n.h.segal@gmail.com>
2022-10-28 12:41:47 +0900

    Add receiveAttack() to Gameboard and unit tests

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-const Ship = (len) => {
+const Ship = (len, xi, yi, _axis) => {
   let _hitNumber = 0;
+  const x = xi;
+  const y = yi;
+  const axis = _axis;
   let length = len;
   const hit = () => {
     _hitNumber = _hitNumber+1;
@@ -10,6 +13,9 @@ const Ship = (len) => {
   };
   return {
     length,
+    x,
+    y,
+    axis,
     get hitNumber() {return _hitNumber},
     get sunk() {return _sunk},
     hit,
@@ -17,11 +23,40 @@ const Ship = (len) => {
   };
 };
 
-let testShip = Ship(4);
-console.log(testShip.hitNumber)
-testShip.hit()
-//console.log(testShip)
+const Gameboard = () => {
+  const myShips =[];
+  const placeShip = (len, xi, yi, axis) => {
+    const newShip = Ship(len, xi, yi, axis);
+    myShips.push(newShip);
+    return newShip;
+  }
+
+  const shotRecord = [];
+
+  const receiveAttack = (x,y) => {
+    shotRecord.push([x,y])
+    myShips.forEach((ship) => {
+      if (ship.axis === 'x') {
+        if (x >= ship.x && x < ship.x + ship.length && y === ship.y) {
+          ship.hit();
+        }
+      }
+      else if (ship.axis === 'y') {
+        if (x === ship.x && y >= ship.y && y < ship.y + ship.length) {
+          ship.hit();
+        }
+      }
+    })
+  }
+  return {
+    myShips,
+    placeShip,
+    shotRecord,
+    receiveAttack
+  }
+}
 
 module.exports = {
-  Ship
+  Ship,
+  Gameboard
 };
