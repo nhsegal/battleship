d1e18dccdfd147b8915895e6aa7ea1f04d344517
Noah Segal <n.h.segal@gmail.com>
2022-10-28 19:21:16 +0900

    Add allSunk() and its unit tests to gameboard

--- game.js
+++ game.js
@@ -5,58 +5,75 @@ const Ship = (len, xi, yi, _axis) => {
   const axis = _axis;
   let length = len;
   const hit = () => {
-    _hitNumber = _hitNumber+1;
+    _hitNumber = _hitNumber + 1;
   };
   const isSunk = () => {
-    if (_hitNumber >= length) { return true}
-    return false
+    if (_hitNumber >= length) {
+      return true;
+    }
+    return false;
   };
   return {
     length,
     x,
     y,
     axis,
-    get hitNumber() {return _hitNumber},
-    get sunk() {return _sunk},
+    get hitNumber() {
+      return _hitNumber;
+    },
     hit,
-    isSunk
+    isSunk,
   };
 };
 
 const Gameboard = () => {
-  const myShips =[];
+  const boardLength = 10;
+  const myShips = [];
   const placeShip = (len, xi, yi, axis) => {
+    if (xi < 0 || xi > boardLength - 1 || yi < 0 || yi > boardLength - 1) {
+      throw new Error("Location out of bounds");
+    }
+    if (
+      (axis === "x" && xi > boardLength - 1 - len) ||
+      (axis === "y" && yi > boardLength - 1 - len)
+    ) {
+      throw new Error("Ship partially out of bounds");
+    }
     const newShip = Ship(len, xi, yi, axis);
     myShips.push(newShip);
     return newShip;
-  }
+  };
 
   const shotRecord = [];
-
-  const receiveAttack = (x,y) => {
-    shotRecord.push([x,y])
+  const receiveAttack = (x, y) => {
+    shotRecord.push([x, y]);
     myShips.forEach((ship) => {
-      if (ship.axis === 'x') {
+      if (ship.axis === "x") {
         if (x >= ship.x && x < ship.x + ship.length && y === ship.y) {
           ship.hit();
         }
-      }
-      else if (ship.axis === 'y') {
+      } else if (ship.axis === "y") {
         if (x === ship.x && y >= ship.y && y < ship.y + ship.length) {
           ship.hit();
         }
       }
-    })
-  }
+    });
+  };
+  const allSunk = () => {
+    return myShips.every((ship) => ship.isSunk())
+  };
+
+
   return {
     myShips,
     placeShip,
     shotRecord,
-    receiveAttack
-  }
-}
+    receiveAttack,
+    allSunk,
+  };
+};
 
 module.exports = {
   Ship,
-  Gameboard
+  Gameboard,
 };
--- game.test.js
+++ game.test.js
@@ -31,33 +31,66 @@ describe("Ship factory", () => {
 describe("Gameboard factory", () => {
   it("Should return a ship with correct position", () => {
     let testGameboard = game.Gameboard();
-    let testShip = testGameboard.placeShip(3, 1, 2, 'y');
+    let testShip = testGameboard.placeShip(3, 1, 2, "y");
     expect(testShip.x).toBe(1);
     expect(testShip.y).toBe(2);
   });
- it("Should register received attacks on head of ship", () =>{
+
+  it("Should throw an error when ship placed out of bounds", () => {
     let testGameboard = game.Gameboard();
-    testGameboard.placeShip(4, 3, 2, 'x');
-    testGameboard.receiveAttack(3,2);
-    expect(testGameboard.shotRecord[0]).toEqual([3,2]);
+    expect(() => testGameboard.placeShip(4, -11, 2, "y")).toThrow(
+      "Location out of bounds"
+    );
+    expect(() =>
+      testGameboard
+        .placeShip(3, 9, 2, "x")
+        .toThrow("Ship partially out of bounds")
+    );
+  });
+
+  it("Should register received attacks on head of ship", () => {
+    let testGameboard = game.Gameboard();
+    testGameboard.placeShip(4, 3, 2, "x");
+    testGameboard.receiveAttack(3, 2);
+    expect(testGameboard.shotRecord[0]).toEqual([3, 2]);
     expect(testGameboard.myShips[0].hitNumber).toBe(1);
-  })
-  it("Should register a missed shot", () =>{
+  });
+  it("Should register a missed shot", () => {
     let testGameboard = game.Gameboard();
     testGameboard.placeShip(4, 3, 2);
-    testGameboard.receiveAttack(4,2);
-    expect(testGameboard.shotRecord[0]).toEqual([4,2]);
+    testGameboard.receiveAttack(4, 2);
+    expect(testGameboard.shotRecord[0]).toEqual([4, 2]);
     expect(testGameboard.myShips[0].hitNumber).toBe(0);
-  })
-  it("Should register received attacks on body of ship", () =>{
+  });
+  it("Should register received attacks on body of ship", () => {
     let testGameboard = game.Gameboard();
-    testGameboard.placeShip(4, 3, 2, 'x');
-    testGameboard.placeShip(3, 1, 1, 'y');
-    testGameboard.receiveAttack(5,2);
-    testGameboard.receiveAttack(1,2);
-    expect(testGameboard.shotRecord[0]).toEqual([5,2]);
-    expect(testGameboard.myShips[0].hitNumber).toBe(1);
-    expect(testGameboard.shotRecord[1]).toEqual([1,2]);
+    testGameboard.placeShip(4, 3, 2, "x");
+    testGameboard.placeShip(3, 1, 1, "y");
+    testGameboard.receiveAttack(5, 2);
+    testGameboard.receiveAttack(4, 2);
+    testGameboard.receiveAttack(1, 2);
+    expect(testGameboard.shotRecord[0]).toEqual([5, 2]);
+    expect(testGameboard.myShips[0].hitNumber).toBe(2);
+    expect(testGameboard.shotRecord[1]).toEqual([4, 2]);
     expect(testGameboard.myShips[1].hitNumber).toBe(1);
+  });
+
+  it("Should report whether all are sunk", () => {
+    let testGameboard = game.Gameboard();
+    testGameboard.placeShip(4, 3, 2, "x");
+    testGameboard.placeShip(3, 1, 1, "y");
+
+    testGameboard.receiveAttack(3, 2);
+    testGameboard.receiveAttack(4, 2);
+   testGameboard.receiveAttack(5, 2);
+    testGameboard.receiveAttack(6, 2);
+    expect(testGameboard.allSunk()).toBe(false);
+    
+    testGameboard.receiveAttack(1, 1);
+    testGameboard.receiveAttack(1, 2);
+    testGameboard.receiveAttack(1, 3);
+
+    expect(testGameboard.allSunk()).toBe(true);
+
   })
 });
